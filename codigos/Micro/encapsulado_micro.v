891b8ba335b627d80aacc842c1fcd40eeec67b22
Kmena <kmena281@gmail.com>
2016-10-18 18:19:16 -0600

    encapsulado micro

---
+++ codigos/Micro/encapsulado_micro.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:36:54 10/18/2016 
+// Design Name: 
+// Module Name:    encapsulado_micro 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module encapsulado_micro(
+	output [7:0]		out_port,
+	input	[7:0]		in_port,
+	output  writestrobe,
+	output			read_strobe,
+	output			interrupt,          //See note above
+	input			interrupt_ack,
+	input clk,
+	input kcpsm6_reset,
+	output actRTC, 
+	output actVGA, 
+	output actTeclado,
+	output actsonido, 
+	output dir
+   );
+	wire [7:0]		port_id;
+	wire write_strobe;
+	wire k_write_strobe;
+	
+	or2(.dato1(write_strobe),.dato2(k_write_strobe),.salida(writestrobe));
+	pico piko(.port_id(port_id),.out_port(out_port),.in_port(in_port),.write_strobe(write_strobe),.k_write_strobe(k_write_strobe),.read_strobe(read_strobe),.interrupt(interrupt),.interrupt_ack(interrupt_ack),.clk(clk),.kcpsm6_reset(kcpsm6_reset));
+	deco_id id_out(.id_port(port_id), .actRTC(actRTC), .actVGA(actVGA), .actTeclado(actTeclado), .actsonido(actsonido), .dir(dir));
+	
+endmodule
